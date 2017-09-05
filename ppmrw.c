ee7e460db916cbdd9ef14e6865c5ba93f9488f8b
Trey <ttangeman97@gmail.com>
2017-09-05 15:54:35 -0700

    Changed format check in main from a logical OR to a logical AND. Moved the file stuff after the format checking.Started processing the file by creating a function that reads the file into memory and returns the pointer and the size.

--- ppmrw.c
+++ ppmrw.c
@@ -12,32 +12,65 @@
 #include "ppmrw.h"
 
 /*
- * Entry point for the program
- * Mainly does usage checking and setup for file input/output
+ * No need to include this in the header because it is only used
+ * for the static function "get_file_contents"
+ */
+struct file_contents {
+    void *memory;
+    size_t size;
+};
+
+/*
+ * Reads a file into memory and returns a struct with a pointer
+ * to the memory and the file size.
+ */
+static struct file_contents get_file_contents(FILE *fh)
+{
+    void *memory;
+    size_t size;
+
+    fseek(fh, 0, SEEK_END);
+    size = ftell(fh);
+    rewind(fh);
+
+    memory = malloc(size);
+    fwrite(memory, 1, size, fh);
+
+    struct file_contents result = {memory, size};
+    return result;
+}
+
+/*
+ * ---- Entry point for the program ----
+ * Usage and error checking is done in the main function, before
+ * it begins processing the file.
  */
 int main(int argc, char **argv)
 {
     if (argc != 4) {
-        fprintf(stderr, "Usage: %s [3 or 6] [input] [output]\n", argv[0]);
-        exit(1);
+        fprintf(stderr, "Usage: %s [3|6] [input] [output]\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    int format = atoi(argv[1]);
+    s32 format = atoi(argv[1]);
     char *in_fname = argv[2];
     char *out_fname = argv[3];
 
-    FILE *input = fopen(in_fname, "r");
-
-    if (format != PP3_PPM || format != PP6_PPM) {
+    if (format != P3_PPM && format != P6_PPM) {
         fprintf(stderr, "Error: invalid format \"%d\" specified!\n", format);
-        fprintf(stderr, "Valid formats: 3, 6\n");
-        exit(1);
+        fprintf(stderr, "Valid formats: 3|6\n");
+        exit(EXIT_FAILURE);
     }
+
+    FILE *input = fopen(in_fname, "r");
 
     if (!input) {
         fprintf(stderr, "Error: unable to open input file %s!\n", in_fname);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
+
+    struct file_contents fc = get_file_contents(input);
+    fclose(in_fname);
 
     return 0;
 }
