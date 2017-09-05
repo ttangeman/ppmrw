215d9f808242648d9ee9604a60a4e44c1a757683
Trey <ttangeman97@gmail.com>
2017-09-05 15:21:20 -0700

    Added some convenient typedefs for integers and created a struct for pixels and ppm files.

--- ppmrw.h
+++ ppmrw.h
@@ -1,6 +1,27 @@
 #pragma once
 
-enum FileType {
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t   s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+typedef int64_t  s64;
+
+enum ppm_format {
     PP3_PPM = 3,
     PP6_PPM = 6
 };
+
+struct pixel {
+    u8 r, g, b, a;
+};
+
+struct ppm {
+    enum ppm_format format;
+    u32 width, height, color_range;
+    struct pixel *pixmap;
+};
+
