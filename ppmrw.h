fb4494e1743324ee67e69839a0dc62e131d56cf1
Trey <ttangeman97@gmail.com>
2017-09-06 10:33:19 -0700

    Changed formatting of ppm_pixmap struct.

--- ppmrw.h
+++ ppmrw.h
@@ -1,14 +1,14 @@
 #pragma once
 
-typedef uint8_t  u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef uint8_t     u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
+typedef uint64_t    u64;
 
-typedef int8_t   s8;
-typedef int16_t  s16;
-typedef int32_t  s32;
-typedef int64_t  s64;
+typedef int8_t      s8;
+typedef int16_t     s16;
+typedef int32_t     s32;
+typedef int64_t     s64;
 
 enum ppm_format {
     P1_PPM = 1,
@@ -25,8 +25,9 @@ struct pixel {
 };
 
 struct ppm_pixmap {
-    enum ppm_format format;
-    u32 width, height, color_range;
+    enum ppm_format format;     // P3 or P6 for pixmaps
+    u32 width, height;
+    u32 max_color_depth;        // 0 to max_color_depth for RGB
     struct pixel *pixmap;
 };
 
